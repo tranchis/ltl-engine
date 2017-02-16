029395dc85abd74e4da646a82d82534ad5f4c268
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-02-16 03:12:55 +0100

    First half of the algorithm

---
+++ src/ltl_engine/core.clj
@@ -0,0 +1,109 @@
+(ns ltl-engine.core
+  (:require [instaparse.core :as insta]
+            [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
+
+(def ltl-parser
+  (insta/parser
+   "S := T | TRUE | FALSE | N | A | O | I | U | R | X | F | G | <'('> <SEP> S <SEP> <')'>
+    TRUE := <'TRUE'>
+    FALSE := <'FALSE'>
+    N := <'NOT'> <W> S
+    A := S <W> <'AND'> <W> S
+    O := S <W> <'OR'> <W> S
+    I := S <W> <'IMPLIES'> <W> S
+    U := S <W> <'UNTIL'> <W> S
+    R := S <W> <'RELEASE'> <W> S
+    X := <'NEXT'> <W> S
+    F := <'EVENTUALLY'> <W> S
+    G := <'GLOBALLY'> <W> S
+    SEP := #'\\s*'
+    W := #'\\s+' 
+    T := #'[a-z]+'"))
+
+(defn parse-ltl [s] (insta/parse ltl-parser s))
+
+(defmulti tree->tree first)
+
+(defmethod tree->tree nil [_] [])
+
+(defmethod tree->tree :T [[_ t]] t)
+
+(defmethod tree->tree :S [[_ s]] (tree->tree s))
+
+(defmethod tree->tree :F [[_ s]] [:U :TRUE (tree->tree s)])
+
+(defmethod tree->tree :G [[_ s]] [:R :FALSE (tree->tree s)])
+
+(defmethod tree->tree :I [[_ s1 s2]] [:O [:N (tree->tree s1)] (tree->tree s2)])
+
+(defmethod tree->tree :default [[k & ltls]]
+  (concat [k] (into [] (map tree->tree ltls))))
+
+(defmulti cl first)
+
+(defn negate [ltl]
+  (let [[k & ss] ltl]
+    (condp = k
+      :N (first ss)
+      :TRUE [:FALSE]
+      :FALSE [:TRUE]
+      [:N ltl])))
+
+(defn cl [ltl]
+  (when-not (= [] ltl)
+    (if (string? ltl)
+      #{ltl (negate ltl)}
+      (let [[k & ss] ltl]
+        (apply set/union #{ltl (negate ltl)} (map cl ss))))))
+
+(defn cl-f [ltl]
+  (set/union #{[:TRUE] [:FALSE]} (cl ltl)))
+
+(defn check-ors [cl-set m f]
+  (let [all-ors (into #{}
+                      (filter #(and (= (first %) :O)
+                                    (or (= (second %) f)
+                                        (= (nth % 2) f)))
+                              cl-set))]
+    (= all-ors (set/intersection all-ors m))))
+
+(defn is-maximal? [cl-set c]
+  (let [m (into #{} c)
+        has-true? (contains? m [:TRUE])
+        r-and (fn [p n] (and p n))
+        either? (reduce r-and true (map #(or
+                                          (contains? m %)
+                                          (contains? m (negate %)))
+                                        cl-set))
+        no-negate? (reduce r-and true (map #(not (contains? m (negate %))) m))
+        fulfills-and? (reduce r-and true (map #(and (contains? m (second %))
+                                                    (contains? m (nth % 2)))
+                                              (filter #(= :A (first %)) m)))
+        fulfills-reverse-and? (reduce r-and true
+                                      (map #(or
+                                             (and
+                                              (not (contains? cl-set [:A (second %) (first %)]))
+                                              (not (contains? cl-set [:A (first %) (second %)])))
+                                             (contains? m [:A (first %) (second %)])
+                                             (contains? m [:A (second %) (first %)]))
+                                           (combo/combinations (remove #(= :TRUE (first %)) m) 2)))
+        fulfills-or? (reduce r-and true (map #(or (contains? m (second %))
+                                                  (contains? m (nth % 2)))
+                                             (filter #(= :O (first %)) m)))
+        fulfills-reverse-or? (reduce r-and true (map #(check-ors cl-set m %) m))]
+    (and has-true? either? no-negate? fulfills-and? fulfills-or?
+         fulfills-reverse-and? fulfills-reverse-or?)))
+
+(defn cs [cl-set]
+  (let [perms (combo/subsets cl-set)]
+    (filter #(is-maximal? cl-set %) perms)))
+
+#_(defn automata-1 [ltl]
+  )
+
+#_(defn gba [ltl]
+  (let [cl (cl-f ltl)
+        a1 (automata-1 ltl)
+        a2 (automata-2 ltl)]
+    nil))
---
+++ test/ltl_engine/core_test.clj
@@ -0,0 +1,104 @@
+(ns ltl-engine.core-test
+  (:require [clojure.test :refer :all]
+            [ltl-engine.core :refer :all]
+            [midje.sweet :refer :all]))
+
+(deftest ltl-parser-test
+  (testing "Valid conversions"
+    (fact (parse-ltl "a") => [:S [:T "a"]])
+    (fact (parse-ltl "TRUE") => [:S [:TRUE]])
+    (fact (parse-ltl "FALSE") => [:S [:FALSE]])
+    (fact (parse-ltl "NOT a") => [:S [:N [:S [:T "a"]]]])
+    (fact (parse-ltl "(a AND b)") => [:S [:S [:A [:S [:T "a"]] [:S [:T "b"]]]]])
+    (fact (parse-ltl "a IMPLIES b") => [:S [:I [:S [:T "a"]] [:S [:T "b"]]]])
+    (fact (parse-ltl "(a AND b AND c AND d)") =>
+          [:S [:S [:A [:S [:T "a"]]
+                   [:S [:A [:S [:T "b"]]
+                        [:S [:A [:S [:T "c"]]
+                             [:S [:T "d"]]]]]]]]])
+    (fact (parse-ltl "NEXT a") => [:S [:X [:S [:T "a"]]]])
+    (fact (parse-ltl "EVENTUALLY a") => [:S [:F [:S [:T "a"]]]])
+    (fact (parse-ltl "GLOBALLY a") => [:S [:G [:S [:T "a"]]]])
+    (fact (parse-ltl "((a AND b) OR (c AND d))") =>
+          [:S [:S [:O [:S [:S [:A [:S [:T "a"]]
+                               [:S [:T "b"]]]]]
+                   [:S [:S [:A [:S [:T "c"]]
+                            [:S [:T "d"]]]]]]]])
+    (fact (parse-ltl "((a AND TRUE) OR (FALSE AND d))") =>
+          [:S [:S [:O [:S [:S [:A [:S [:T "a"]]
+                               [:S [:TRUE]]]]]
+                   [:S [:S [:A [:S [:FALSE]]
+                            [:S [:T "d"]]]]]]]]))
+  (testing "Invalid conversions"
+    (fact (class (parse-ltl "X a")) => instaparse.gll.Failure)))
+
+(deftest tree->tree-test
+  (testing "Correct pruning"
+    (fact (tree->tree [:S]) => [])
+    (fact (tree->tree [:S [:T "a"]]) => "a")
+    (fact (tree->tree [:S [:N [:S [:T "a"]]]]) => [:N "a"])
+    (fact (tree->tree [:S [:S [:A [:S [:T "a"]] [:S [:T "b"]]]]]) => [:A "a" "b"])
+    (fact (tree->tree [:S [:S [:A [:S [:T "a"]]
+                               [:S [:A [:S [:T "b"]]
+                                    [:S [:A [:S [:T "c"]]
+                                         [:S [:T "d"]]]]]]]]]) =>
+          [:A "a" [:A "b" [:A "c" "d"]]])
+    (fact (tree->tree [:S [:S [:O [:S [:S [:A [:S [:T "a"]]
+                                           [:S [:T "b"]]]]]
+                               [:S [:S [:A [:S [:T "c"]]
+                                        [:S [:T "d"]]]]]]]]) =>
+          [:O [:A "a" "b"] [:A "c" "d"]])
+    (fact (tree->tree [:S [:I [:S [:T "a"]] [:S [:T "b"]]]]) => [:O [:N "a"] "b"])
+    (fact (tree->tree [:S [:F [:S [:T "a"]]]]) => [:U :TRUE "a"])
+    (fact (tree->tree [:S [:G [:S [:T "a"]]]]) => [:R :FALSE "a"])))
+
+(deftest negate-test
+  (fact (negate [:TRUE]) => [:FALSE])
+  (fact (negate [:FALSE]) => [:TRUE])
+  (fact (negate []) => [:N []])
+  (fact (negate "a") => [:N "a"])
+  (fact (negate [:F "a"]) => [:N [:F "a"]])
+  (fact (negate [:N "a"]) => "a"))
+
+(deftest cl-test
+  (fact (cl-f []) => #{[:TRUE] [:FALSE]})
+  (fact (cl-f "a") => #{[:TRUE] [:FALSE] "a" [:N "a"]})
+  (fact (cl-f [:N "a"]) => #{[:TRUE] [:FALSE] "a" [:N "a"]})
+  (fact (cl-f [:X "a"]) => #{[:TRUE] [:FALSE] [:X "a"] [:N [:X "a"]] "a" [:N "a"]})
+  (fact (cl-f [:A "a" "b"]) => #{[:TRUE] [:FALSE] [:A "a" "b"]
+                                 [:N [:A "a" "b"]] "a" [:N "a"] "b" [:N "b"]})
+  (fact (cl-f [:O [:A "a" "b"] [:A "c" "d"]]) => 
+        #{[:TRUE] [:FALSE] [:O [:A "a" "b"] [:A "c" "d"]]
+          [:N [:O [:A "a" "b"] [:A "c" "d"]]]
+          [:A "a" "b"] [:N [:A "a" "b"]] [:A "c" "d"] [:N [:A "c" "d"]]
+          "a" [:N "a"] "b" [:N "b"] "c" [:N "c"] "d" [:N "d"]}))
+
+(deftest is-maximal-test
+  (fact (is-maximal? #{} #{}) => false)
+  (fact (is-maximal? #{[:TRUE]} #{[:TRUE]}) => true)
+  (fact (is-maximal? #{[:TRUE] "a"} #{[:TRUE] "a"}) => true)
+  (fact (is-maximal? #{[:TRUE] "a" [:N "a"]} #{[:TRUE] "a" [:N "a"]}) => false)
+  (fact (is-maximal? #{[:TRUE] [:A "a" "b"]} #{[:TRUE] [:A "a" "b"]}) => false)
+  (fact (is-maximal? #{[:TRUE] [:A "a" "b"] "a"} #{[:TRUE] [:A "a" "b"] "a"}) => false)
+  (fact (is-maximal? #{[:TRUE] [:A "a" "b"] "a" "b"} #{[:TRUE] [:A "a" "b"] "a" "b"}) => true)
+  (fact (is-maximal? #{[:TRUE] [:A "a" "b"] "a" "b"} #{[:TRUE] "a" "b"}) => false)
+  (fact (is-maximal? #{[:TRUE] [:O "a" "b"]} #{[:TRUE] [:O "a" "b"]}) => false)
+  (fact (is-maximal? #{[:TRUE] [:O "a" "b"] "a"} #{[:TRUE] [:O "a" "b"] "a"}) => true)
+  (fact (is-maximal? #{[:TRUE] [:O "a" "b"] "a" "b"} #{[:TRUE] [:O "a" "b"] "a" "b"}) => true)
+  (fact (is-maximal? #{[:TRUE] [:A "a" "b"] "a" "b"} #{[:TRUE] "a" "b"}) => false))
+
+(deftest check-ors-test
+  (fact (check-ors #{[:O "a" "b"] "a" "b"} #{"a"} "a") => false)
+  (fact (check-ors #{[:O "a" "b"] "a" "b"} #{[:O "a" "b"] "a"} "a") => true)
+  (fact (check-ors #{[:O "a" "b"] [:O "a" "c"] "a" "b" "c"} #{[:O "a" "b"] "a"} "a") => false)
+  (fact (check-ors #{[:O "a" "b"] [:O "a" "c"] "a" "b" "c"} #{[:O "a" "b"] [:O "a" "c"] "a"} "a") => true)
+  (fact (check-ors #{[:O "a" "b"] "a" "b"} #{"b"} "d") => true))
+
+(deftest cs-test
+  (fact (cs '()) => '())
+  (fact (cs [[:TRUE] [:FALSE] [:A "a" "b"]
+             [:N [:A "a" "b"]] "a" [:N "a"] "b" [:N "b"]]) =>
+        '(([:TRUE] [:A "a" "b"] "a" "b") ([:TRUE] [:N [:A "a" "b"]] "a" "b")
+          ([:TRUE] [:N [:A "a" "b"]] "a" [:N "b"])
+          ([:TRUE] [:N [:A "a" "b"]] [:N "a"] "b")
+          ([:TRUE] [:N [:A "a" "b"]] [:N "a"] [:N "b"]))))

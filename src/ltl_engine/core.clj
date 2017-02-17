e02f3a879ccc144eb3f46226cfef8658c7d37b97
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-02-17 15:31:51 +0100

    Working version without any optimizations

--- src/ltl_engine/core.clj
+++ src/ltl_engine/core.clj
@@ -81,7 +81,9 @@
         r-and (fn [p n] (and p n))
         either? (reduce r-and true (map #(or
                                           (contains? m %)
-                                          (contains? m (negate %)))
+                                          (and
+                                           (or (= (first %) :N) (string? %))
+                                           (contains? m (negate %))))
                                         cl-set))
         no-negate? (reduce r-and true (map #(not (contains? m (negate %))) m))
         fulfills-and? (reduce r-and true (map #(and (contains? m (second %))
@@ -106,11 +108,149 @@
   (let [perms (combo/subsets (into [] cl-set))]
     (into #{} (filter #(is-maximal? cl-set %) perms))))
 
-#_(defn automata-1 [ltl]
-  )
+(defn positives [ap-s m]
+  (into #{} (filter #(not (contains? m [:N %])) ap-s)))
 
-#_(defn gba [ltl]
-  (let [cl (cl-f ltl)
+(defn fulfills-next? [m m-prima]
+  (let [r-and (fn [p n] (and p n))
+        right true #_(reduce r-and true (map #(contains? m [:X %]) m-prima))
+        left (reduce r-and true (map #(contains? m-prima (second %))
+                                     (filter #(= :X (first %)) m)))]
+    (and right left)))
+
+(defn fulfills-until? [m m-prima]
+  (let [r-and (fn [p n] (and p n))
+        right-1 true #_(reduce
+                 r-and true
+                 (map #(or
+                        (= 0 (count (filter (fn [x] (= :U (first x))) m)))
+                        (> (count (filter (fn [x]
+                                            (and (= :U (first x)) (= % (nth x 2))))
+                                          m))
+                           0))
+                      (remove #(= :U (first %)) m)))
+        right-2
+        #_true (reduce
+                r-and true
+                (map #(and (contains? m (second %))
+                           (contains? m %))
+                     (filter #(= :U (first %)) m-prima)))
+        left (reduce
+              r-and true
+              (map #(or (contains? m (nth % 2))
+                        (and (contains? m (second %))
+                             (contains? m-prima %)))
+                   (filter #(= :U (first %)) m)))]
+    (and right-1 right-2 left)))
+
+(defn fulfills-release? [m m-prima]
+  (let [r-and (fn [p n] (and p n))
+        right-1 true #_(reduce
+                        r-and true
+                        (map #(> (count (filter (fn [x]
+                                                  (and (= :U (first x)) (= % (nth x 2))))
+                                                m))
+                                 0)
+                             (remove #(= :U (first %)) m)))
+        right-2 true #_(reduce
+                        r-and true
+                        (map #(and (contains? m (second %))
+                                   (contains? m %))
+                             (filter #(= :U (first %)) m-prima)))
+        left (reduce
+              r-and true
+              (map #(or (contains? m [:A (second %) (nth % 2)])
+                        (and (contains? m (nth % 2))
+                             (contains? m-prima %)))
+                   (filter #(= :R (first %)) m)))]
+    (and right-1 right-2 left)))
+
+(defn subset-ap? [ap-s m-prima a]
+  (let [intersect (set/intersection m-prima ap-s)
+        subset-a? (set/subset? intersect a)
+        positives (positives ap-s m-prima)
+        subset-p? (set/subset? a positives)]
+    (and subset-a? subset-p?)))
+
+(defn valid-trans? [ap-s {:keys [m a m-prima]}]
+  (let [ss? (subset-ap? ap-s m-prima a)
+        iff-x? (fulfills-next? m m-prima)
+        iff-u? (fulfills-until? m m-prima)
+        iff-r? (fulfills-release? m m-prima)]
+    (and ss? iff-x? iff-u? iff-r?)))
+
+(defn automata-1 [ltl]
+  (let [cl-s (cl-f ltl)
+        cs-s (cs cl-s)
+        ap (filter string? cl-s)
+        ap-s (into #{} ap)
+        ap-2 (combo/subsets ap)
+        m-s (mapcat #(vector % (reverse %)) (combo/combinations cs-s 2))
+        mam-s (map #(hash-map :m (into #{} (first (first %)))
+                              :a (into #{} (second %))
+                              :m-prima (into #{} (second (first %))))
+                   (combo/cartesian-product m-s ap-2))
+        valid-trans (filter #(valid-trans? ap-s %) mam-s)]
+    valid-trans))
+
+(defn automata-2 [ltl]
+  (let [init ltl
+        cl-s (cl-f ltl)
+        cs-s (cs cl-s)
+        ap (filter string? cl-s)
+        ap-s (into #{} ap)
+        ap-2 (combo/subsets ap)
+        mam-s (map #(hash-map :m init
+                              :a (into #{} (second %))
+                              :m-prima (into #{} (first %)))
+                   (combo/cartesian-product cs-s ap-2))
+        valid-trans (filter #(and
+                              (contains? (:m-prima %) ltl)
+                              (subset-ap? ap-s (:m-prima %) (:a %))) mam-s)]
+    valid-trans))
+
+(defn f-set [ltl]
+  (let [cl-s (cl-f ltl)
+        cs-s (cs cl-s)]
+    (into #{}
+          (map
+           #(into #{} (filter (fn [m-s]
+                                (or
+                                 (contains? m-s (nth % 2))
+                                 (contains? m-s [:N %])))
+                              (map (fn [x] (into #{} x)) cs-s)))
+           (filter #(= :U (first %)) cl-s)))))
+
+(defn gba [ltl]
+  (let [cl-s (cl-f ltl)
+        cs-s (cs cl-s)
         a1 (automata-1 ltl)
-        a2 (automata-2 ltl)]
-    nil))
+        a2 (automata-2 ltl)
+        ap (filter string? cl-s)
+        ap-s (into #{} ap)
+        ap-2 (combo/subsets ap)]
+    {:state-space (set/union #{ltl} cs-s)
+     :propositions ap-2
+     :transitions (into #{} (set/union a1 a2))
+     :states #{ltl}
+     :f (f-set ltl)}))
+
+(defn transitions [gba propositions]
+  (let [states (:states gba)
+        p-s (into #{} propositions)
+        transitions (into #{}
+                          (mapcat
+                           #(filter
+                             (fn [x]
+                               (and (= (:m x) %)
+                                    (= (:a x) p-s)))
+                             (:transitions gba))
+                           states))]
+    transitions))
+
+(defn advance [gba propositions]
+  (let [destinations (map :m-prima (transitions gba propositions))]
+    (assoc gba :states (into #{} destinations))))
+
+#_(let [g (gba [:U "a" "b"])]
+  (advance (advance (advance g ["a"]) ["b"]) []))
--- test/ltl_engine/core_test.clj
+++ test/ltl_engine/core_test.clj
@@ -1,7 +1,8 @@
 (ns ltl-engine.core-test
   (:require [clojure.test :refer :all]
             [ltl-engine.core :refer :all]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer :all]
+            [clojure.math.combinatorics :as combo]))
 
 (deftest ltl-parser-test
   (testing "Valid conversions"
@@ -96,7 +97,7 @@
   (fact (check-ors #{[:O "a" "b"] "a" "b"} #{"b"} "d") => true))
 
 (deftest cs-test
-  (fact (cs #{}) => '())
+  (fact (cs #{}) => #{})
   (fact (into '() (cs #{[:TRUE] [:FALSE] [:O "a" "b"]
                         [:N [:O "a" "b"]] "a" [:N "a"] "b" [:N "b"]})) =>
         '(([:N "b"] [:N "a"] [:N [:O "a" "b"]] [:TRUE])
@@ -109,3 +110,76 @@
           ([:N "b"] [:N "a"] [:N [:A "a" "b"]] [:TRUE])
           ([:N "a"] [:N [:A "a" "b"]] "b" [:TRUE])
           ([:A "a" "b"] "a" "b" [:TRUE]))))
+
+(deftest positives-test
+  (fact (positives #{"a" "b" "c"} #{"a"}) => #{"a" "b" "c"})
+  (fact (positives #{"a" "b" "c"} #{"a" [:N "b"] [:N "c"]}) => #{"a"}))
+
+(deftest fulfills-next-test
+  (fact (fulfills-next? #{[:X "a"]} #{"a"}) => true)
+  (fact (fulfills-next? #{[:X "a"]} #{"b"}) => false)
+  (fact (fulfills-next? #{[:X [:N "a"]]} #{[:N "a"]}) => true)
+  (fact (fulfills-next? #{} #{"a"}) => true))
+
+(deftest fulfills-until-test
+  (fact (fulfills-until? #{[:U "a" "b"]} #{}) => false)
+  (fact (fulfills-until? #{[:U "a" "b"] "b"} #{}) => true)
+  (fact (fulfills-until? #{[:U "a" "b"] "a"} #{}) => false)
+  (fact (fulfills-until? #{[:U "a" "b"] "a"} #{[:U "a" "b"]}) => true)
+  (fact (fulfills-until? #{[:U "a" "b"] "a" "b"} #{}) => true))
+
+(deftest fulfills-release-test
+  (fact (fulfills-release? #{[:R "a" "b"]} #{}) => false)
+  (fact (fulfills-release? #{[:R "a" "b"] [:A "a" "b"]} #{}) => true)
+  (fact (fulfills-release? #{[:R "a" "b"] [:A "c" "b"]} #{}) => false)
+  (fact (fulfills-release? #{[:R "a" "b"] "b"} #{}) => false)
+  (fact (fulfills-release? #{[:R "a" "b"] "b"} #{}) => false)
+  (fact (fulfills-release? #{[:R "a" "b"] "b"} #{[:R "a" "b"]}) => true))
+
+(deftest valid-trans-test
+  (fact (valid-trans? #{"a" "b" "c"}
+                      {:m #{[:X "a"] [:X "b"] [:X "c"]}
+                       :a #{"a" "b" "c"}
+                       :m-prima #{"a" "b" "c"}})
+        => true)
+  (fact (valid-trans? #{"a" "b" "c"}
+                      {:m #{[:X "a"] [:X "b"] [:X "c"]}
+                       :a #{"a" "b"}
+                       :m-prima #{"a" "b" "c"}})
+        => false)
+  (fact (valid-trans? #{"a" "b" "c"}
+                      {:m #{[:X "a"] [:X [:N "b"]] [:X [:N "c"]]}
+                       :a #{"a" "b"}
+                       :m-prima #{"a" [:N "b"] [:N "c"]}})
+        => false)
+  (fact (valid-trans? #{"a" "b" "c"}
+                      {:m #{[:X "a"] [:X "b"] [:X [:N "c"]]}
+                       :a #{"a" "b"}
+                       :m-prima #{"a" "b" [:N "c"]}})
+        => true)
+  (fact (valid-trans? #{"a" "b"}
+                      {:m #{[:X "a"]}
+                       :a #{"a"}
+                       :m-prima #{"a"}}) => true))
+
+(deftest automata-1-test
+  (fact (automata-1 [:A "a" "b"]) => (contains
+                                      {:m-prima #{[:A "a" "b"] "a" "b" [:TRUE]},
+                                       :m #{[:N "a"] [:N [:A "a" "b"]] "b" [:TRUE]},
+                                       :a #{"a" "b"}}))
+  (fact (automata-1 [:U "a" "b"]) => (contains
+                                      {:m-prima #{[:N "a"] "b" [:TRUE] [:U "a" "b"]},
+                                       :m #{[:N [:U "a" "b"]] [:N "b"] [:N "a"] [:TRUE]},
+                                       :a #{"b"}})))
+
+(deftest f-set-test
+  (fact (f-set "a") => #{})
+  (fact (f-set [:U "a" "b"]) => 
+        #{#{#{[:N [:U "a" "b"]] "a" "b" [:TRUE]}
+            #{[:N [:U "a" "b"]] [:N "a"] "b" [:TRUE]}
+            #{"a" "b" [:TRUE] [:U "a" "b"]}
+            #{[:N "a"] "b" [:TRUE] [:U "a" "b"]}
+            #{[:N [:U "a" "b"]] [:N "b"] "a" [:TRUE]}
+            #{[:N [:U "a" "b"]] [:N "b"] [:N "a"] [:TRUE]}}}))
+
+
